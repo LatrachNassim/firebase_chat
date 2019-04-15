c9b32e9821df1fd26e091949f4e22e6a8518f629
Latrach Nassim <latrach.nassim@gmail.com>
2019-04-15 12:04:03 +0200

    Installation du vanilla-router

---
+++ controllers/Chat.js
@@ -0,0 +1,12 @@
+export default class Chat {
+    
+    constructor(router) {
+        this.router = router;
+        this.view = 'chat.html';
+    }
+
+    init() {
+        console.log('CHAT !');
+    }
+
+}
